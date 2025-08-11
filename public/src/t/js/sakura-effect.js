af632b1ec43809174b2ce80fccd5ac7dd345b9fc
Jerry👨‍💻 <jeban487@gmail.com>
2025-08-11 15:07:46 +0700

    Update and rename app.js to sakura-effect.js

---
+++ public/src/t/js/sakura-effect.js
@@ -0,0 +1,96 @@
+particlesJS('particles-js', {
+  "particles": {
+    "number": {
+      "value": 30,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "shape": {
+      "type": "image",
+      "image": {
+        "src": "https://i.ibb.co/2qGkTLP/sakura-petal.png", // Cánh hoa PNG nền trong suốt
+        "width": 50,
+        "height": 50
+      }
+    },
+    "opacity": {
+      "value": 0.9,
+      "random": true
+    },
+    "size": {
+      "value": 12,
+      "random": true
+    },
+    "line_linked": {
+      "enable": false
+    },
+    "move": {
+      "enable": true,
+      "speed": 1.5,
+      "direction": "bottom",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "angle": {
+        "value": 45,
+        "offset": 20
+      },
+      "attract": {
+        "enable": false
+      }
+    },
+    "rotate": {
+      "value": 30,
+      "random": true,
+      "direction": "random",
+      "animation": {
+        "enable": true,
+        "speed": 5
+      }
+    },
+    "tilt": {
+      "enable": true,
+      "value": 0,
+      "direction": "random",
+      "animation": {
+        "enable": true,
+        "speed": 2
+      }
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": { "enable": false },
+      "onclick": { "enable": false }
+    }
+  },
+  "retina_detect": true
+});        "distance": 150,
+        "line_linked": {
+          "opacity": 0.8
+        }
+      },
+      "bubble": {
+        "distance": 300,
+        "size": 8,
+        "duration": 1,
+        "opacity": 6,
+        "speed": 6
+      },
+      "repulse": {
+        "distance": 250
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true,
+}
+
+);
